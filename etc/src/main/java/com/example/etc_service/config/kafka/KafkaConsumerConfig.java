a71eda70c97f0f47c50fae1eea4a630863ae703f
goeom77 <7eom14@gmail.com>
2023-11-09 17:39:04 +0900

    Error yaml setting add

--- etc/src/main/java/com/example/etc_service/config/kafka/KafkaConsumerConfig.java
+++ etc/src/main/java/com/example/etc_service/config/kafka/KafkaConsumerConfig.java
@@ -1,12 +1,14 @@
 package com.example.etc_service.config.kafka;
 
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -25,6 +27,13 @@ public class KafkaConsumerConfig {
         return new DefaultKafkaConsumerFactory<>(properties);
     }
 
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("test")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         // kafka listener container factory
