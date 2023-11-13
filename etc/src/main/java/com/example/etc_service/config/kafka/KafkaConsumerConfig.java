f35427a38e23d717f510df511eb7035c5debafaa
goeom77 <7eom14@gmail.com>
2023-11-13 18:07:22 +0900

    Test kafka connect test

--- etc/src/main/java/com/example/etc_service/config/kafka/KafkaConsumerConfig.java
+++ etc/src/main/java/com/example/etc_service/config/kafka/KafkaConsumerConfig.java
@@ -1,9 +1,12 @@
 package com.example.etc_service.config.kafka;
 
 
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -11,16 +14,25 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${spring.consumers.bootstrap-servers}")
+    private String bootstrapServers;
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.64.149.128:9092"); // kafka container host
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // kafka container host
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "etc-service"); // consumer group id 그룹으로 지정해서 보내기 가능
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // key deserializer
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class); // value deserializer
@@ -30,7 +42,7 @@ public class KafkaConsumerConfig {
     @Bean
     public NewTopic topic() {
         return TopicBuilder.name("test")
-                .partitions(10)
+                .partitions(1)
                 .replicas(1)
                 .build();
     }
