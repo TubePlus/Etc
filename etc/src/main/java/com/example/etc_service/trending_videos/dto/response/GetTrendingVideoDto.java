c852b692938c6def2ee8455fb461ced135c82aa6
kklee0930 <kklee0930@gmail.com>
2023-11-27 21:26:00 +0900

    FIX redis entity annotation added
    redis 데이터 불러오기 에러 해결을 위한 JsonProperty, JsonTypeInfo 어노테이션 추가

--- etc/src/main/java/com/example/etc_service/trending_videos/dto/response/GetTrendingVideoDto.java
+++ etc/src/main/java/com/example/etc_service/trending_videos/dto/response/GetTrendingVideoDto.java
@@ -1,6 +1,8 @@
 package com.example.etc_service.trending_videos.dto.response;
 
 import com.example.etc_service.trending_videos.domain.CategoryType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.*;
 
 @Getter
@@ -9,12 +11,24 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "@class"
+)
 public class GetTrendingVideoDto {
+
+    @JsonProperty("title")
     private String videoTitle; // title
+    @JsonProperty("ranking")
     private Long ranking; //
+    @JsonProperty("video_category")
     private CategoryType videoCategory; // categoryId
+    @JsonProperty("thumbnail_url")
     private String thumbnailUrl; // thumbnails.standard
+    @JsonProperty("channel_name")
     private String channelName; // channelTitle
+    @JsonProperty("channel_id")
     private String channelId; // id
+    @JsonProperty("video_url")
     private String videoUrl; // player.embedHtml
 }
