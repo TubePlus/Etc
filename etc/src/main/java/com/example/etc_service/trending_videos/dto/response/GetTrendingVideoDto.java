efc35ac985725276e21e33eeab64d5a7f1ad67a1
kklee0930 <kklee0930@gmail.com>
2023-11-27 21:42:53 +0900

    FIX jsonproperty annotations removed
    json property annotation 제거했습니다.

--- etc/src/main/java/com/example/etc_service/trending_videos/dto/response/GetTrendingVideoDto.java
+++ etc/src/main/java/com/example/etc_service/trending_videos/dto/response/GetTrendingVideoDto.java
@@ -11,24 +11,13 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "@class"
-)
 public class GetTrendingVideoDto {
 
-    @JsonProperty("title")
     private String videoTitle; // title
-    @JsonProperty("ranking")
     private Long ranking; //
-    @JsonProperty("video_category")
     private CategoryType videoCategory; // categoryId
-    @JsonProperty("thumbnail_url")
     private String thumbnailUrl; // thumbnails.standard
-    @JsonProperty("channel_name")
     private String channelName; // channelTitle
-    @JsonProperty("channel_id")
     private String channelId; // id
-    @JsonProperty("video_url")
     private String videoUrl; // player.embedHtml
 }
